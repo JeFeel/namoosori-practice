1555bcd371f39b24a47a580a307cf7336a25da79
JeFeel <jaephil94@gmail.com>
2023-10-08 19:25:06 +0900

    [feat] StoreLifeCycler 추가

--- TravelClub/src/io/namoosori/java/travelClub/store/StoreLifeCycler.java
+++ TravelClub/src/io/namoosori/java/travelClub/store/StoreLifeCycler.java
@@ -1,6 +1,31 @@
 package io.namoosori.java.travelClub.store;
 
 
+import io.namoosori.java.travelClub.store.logic.ClubMapStore;
+
+
 public class StoreLifeCycler {
+//    ServiceLogicLifeCycler와 유사하게 ClubStore의 interface를 구현한
+//    ClubMapStore를 제공하게끔 만듦
+
+    private static StoreLifeCycler uniqueInstance; //싱글턴 패턴?
+
+    private final ClubStore clubStore;
+
+    private StoreLifeCycler() {
+        this.clubStore = new ClubMapStore();
+    }
+
+    public static StoreLifeCycler getUniqueInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new StoreLifeCycler();
+        }
+        return uniqueInstance;
+    }
+
+    public ClubStore requestClubStored(){
+        return this.clubStore;
+    }
+
 
 }
